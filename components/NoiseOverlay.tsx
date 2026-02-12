b24d23be01cde2b59659ddba97ec6b6857206608
joksa <joksafood@gmail.com>
2026-02-12 19:16:08 +0900

    Delete components/NoiseOverlay.tsx

--- components/NoiseOverlay.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export const NoiseOverlay: React.FC = () => {
-  return (
-    <div className="fixed inset-0 pointer-events-none z-[9000] opacity-[0.04] noise-bg"></div>
-  );
-};
